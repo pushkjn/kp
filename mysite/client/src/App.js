ada6d36aae309adbfb911b59b06985645cfe3578
kirill pushkin <k1rilllavrinenko@yandex.ru>
2022-12-21 19:05:10 +0300

    init

---
+++ mysite/client/src/App.js
@@ -0,0 +1,118 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Login from './views/auth/Login';
+import Signup from './views/auth/Signup';
+import Logout from './views/auth/Logout';
+import Dashboard from "./views/app/Dashboard";
+
+import { useState, useEffect } from 'react'
+import './App.css'
+import SingleCard from './components/SingleCard'
+
+
+const cardImages = [
+  { "src": "/img/bluecone_img.png", matched: false },
+  { "src": "/img/bluesquare_img.png", matched: false },
+  { "src": "/img/purplerect_img.png", matched: false },
+  { "src": "/img/ring_img.png", matched: false },
+  { "src": "/img/torus_img.png", matched: false },
+  { "src": "/img/plus_img.png", matched: false },
+]
+
+const App = () => {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+  // shuffle cards for new game
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }))
+
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+  // handle a choice
+  const handleChoice = (card) => {
+    console.log(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+
+    }
+  }, [choiceOne, choiceTwo])
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+
+  // start new game automagically
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
+  return (
+  <div>
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/login' component={Login} exact />
+          <Route path='/signup' component={Signup} exact />
+          <Route path='/logout' component={Logout} exact />
+          <Route path='/dashboard' component={Dashboard} exact />
+        </Switch>
+      </Router>
+    </div>
+
+    <div className='App'>
+      <h1>Memory</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+
+      <p>Turns: {turns}</p>
+    </div>
+  </div>
+  );
+};
+
+export default App;
